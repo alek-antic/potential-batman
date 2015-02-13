5c7e0c0f718a47057e920be9d1fe526393ee3a19
Alek <Alek@aleksandarsimac.attlocal.net>
2015-02-12 23:43:07 -0800

    finally done with crypt

--- Crypt/src/Crypt.java
+++ Crypt/src/Crypt.java
@@ -13,19 +13,20 @@ public class Crypt {
 	private char[][] key = new char[5][5];
 	private int[][] indecies = new int[26][2];
 
-	public void generateKey(String keyword) {
+	private void generateKey(String keyword) {
 
 		char[] noJKey = keyword.toUpperCase().toCharArray();
 		StringBuffer tempKey = new StringBuffer();
 		int[] chars = new int[26];
-		
-		for(char c : noJKey) {
-			if(c=='J')
+
+		for (char c : noJKey) {
+			if (c == 'J')
 				c--;
 			tempKey.append(c);
 		}
 
-		for (int i = 0; i < tempKey.length(); i++) {
+		int tempKeyLen = tempKey.length();
+		for (int i = 0; i < tempKeyLen; i++) {
 			int k = Character.toUpperCase(tempKey.charAt(i)) - 65;
 			if (chars[k] == 0) {
 				chars[k]++;
@@ -35,33 +36,26 @@ public class Crypt {
 		}
 
 		for (int i = 0; i < 26; i++) {
-			if(i == 'J' - 65)
+			if (i == 'J' - 65)
 				continue;
 			if (chars[i] == 0) {
 				tempKey.append((char) (i + 65));
 			}
 		}
 		
-		
 		for (int y = 0; y < 5; y++) {
 			for (int x = 0; x < 5; x++) {
 				key[y][x] = tempKey.charAt(0);
 
-						indecies[key[y][x] - 65][0] = x;
-						indecies[key[y][x] - 65][1] = y;
-			
+				indecies[key[y][x] - 65][0] = x;
+				indecies[key[y][x] - 65][1] = y;
+
 				tempKey.deleteCharAt(0);
 			}
 		}
 	}
 
-	public char[][] getKey() {
-		return key;
-	}
-
-	public int[][] getIndecies() {
-		return indecies;
-	}
+	
 
 	public void encrypt(String inputFileName, String outputFileName,
 			String keyword) {
@@ -92,16 +86,16 @@ public class Crypt {
 			StringBuffer cache2 = new StringBuffer();
 
 			while (in.hasNext()) {
-				char input = in.next().charAt(0);
+				char input = in.next().toCharArray()[0];
 
 				StringBuffer digraph = new StringBuffer();
-				
-				//if char read is j, make it i
+
+				// if char read is j, make it i
 				if (input == 'j' || input == 'J') {
 					input--;
 				}
 
-				//decide which char to fill
+				// decide which char to fill
 				if (counter % 2 == 0) {
 					if (Character.isLetter(input)) {
 						one = input;
@@ -118,41 +112,42 @@ public class Crypt {
 						twoUpper = Character.isUpperCase(two);
 						counter++;
 
-						//Encrypt stuff
-						
+						// Encrypt stuff
+
 						int[] index1 = indecies[Character.toUpperCase(one) - 65];
 						int[] index2 = indecies[Character.toUpperCase(two) - 65];
-						
+
 						char newOne = 0;
 						char newTwo = 0;
-						
+
 						// checks same column or row
-						if(index1[0] == index2[0] || index1[1] == index2[1]) {
+						if (index1[0] == index2[0] || index1[1] == index2[1]) {
 							newOne = Character.toUpperCase(two);
 							newTwo = Character.toUpperCase(one);
 						} else {
 							newOne = key[index1[1]][index2[0]];
 							newTwo = key[index2[1]][index1[0]];
 						}
-						
-						//make lowercase if it should be
-						
-						if(!oneUpper)
+
+						// make lowercase if it should be
+
+						if (!oneUpper)
 							newOne += 32;
-						if(!twoUpper)
+						if (!twoUpper)
 							newTwo += 32;
-							
-						
-						
-						digraph.append(cache1).append(newOne).append(cache2).append(newTwo);
-						//Reset chars and caches
+
+						// link together
+						digraph.append(cache1).append(newOne).append(cache2)
+								.append(newTwo);
+
+						// Reset chars and caches
 						one = 0;
 						two = 0;
 						oneUpper = false;
 						twoUpper = false;
 						cache1.delete(0, cache1.length());
 						cache2.delete(0, cache2.length());
-						
+
 					} else {
 						cache2.append(input);
 						continue;
