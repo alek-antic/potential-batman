ffae62321f2b3c3123c6d95b51c9ceabb782a48b
unknown <aantic235@PR02.hhs.win.fuhsd.org>
2015-02-25 14:15:31 -0800

    gridworld pt3

--- sCTF/src/BigRealEstate/BRE.java
+++ sCTF/src/BigRealEstate/BRE.java
@@ -15,11 +15,36 @@ public class BRE {
 		
 		Plot[] allPlots = new Plot[plots.length];
 		
-		for(String plot : plots) {
-			String[] argues = plot.split("\"");
+		for(int i = 0; i < plots.length; i++) {
+			
+			if(i == 0 || i ==1 || i ==2)
+				continue;
+			
+			String[] argues = plots[i].split("\"");
+			allPlots[i] = new Plot(argues[0], Integer.parseInt(argues[1]),
+							Integer.parseInt(argues[2]), Integer.parseInt(argues[3]));
 		}
 		
+		double bestRatio = 0;
+		String name = "";
+		double secondRatio = 0;
+		String name2 = "";
+		for(Plot p : allPlots) {
+			double ratio = p.getPriceToAreaRatio();
+			if(ratio > bestRatio) {
+				secondRatio = bestRatio;
+				bestRatio = ratio;
+				name2 = name;
+				name = p.toString();
+				continue;
+			}
+			if(ratio > secondRatio) {
+				secondRatio = ratio;
+				name2 = p.toString();
+			}
+		}
 		
+		System.out.println(name + " " + name2);
 		
 	}
 	
