ae3baa1b5ce403af90978077ab23ba2a269f6ef7
unknown <aantic235@PR05.hhs.win.fuhsd.org>
2015-03-30 14:26:05 -0700

    numberline shenanigans

--- NumberLinePart2/src/GUI.java
+++ NumberLinePart2/src/GUI.java
@@ -7,13 +7,22 @@ public class GUI extends JFrame {
 	
 	
 	private BorderLayout border;
-	private JPanel widgets, numberLine;
+	private Drawing numberLine;
+	private BottomPanel bottomPanel;
 	
 	
 	public GUI() {
+		super("Number Line");
 		border = new BorderLayout();
-		numberLine = new NumberLine();
+		setLayout(border);
+		bottomPanel = new BottomPanel();
+		numberLine = new Drawing(bottomPanel);
 		
 		
+		setBounds(100, 100, 640, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(numberLine, BorderLayout.CENTER);
+		add(bottomPanel, BorderLayout.SOUTH);
+		setResizable(true);
 	}
 }
--- NumberLinePart2/src/NumberLine.java
+++ NumberLinePart2/src/NumberLine.java
@@ -1,53 +1,20 @@
-import java.awt.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-import javax.swing.*;
 
-/**
- * TODO Write a one-sentence summary of your class here. Follow it with
- * additional details about its purpose, what abstraction it represents, and how
- * to use it.
- * 
- * @author Your Name
- * @version Date
- * 
- *          Period - Your Period 
- *          Assignment - Name of Assignment
- *          Sources - List collaborators
- */
-public class NumberLine extends JPanel {
-	// TODO Your Instance Variables Here
+public class NumberLine {
+
 	
-	
-	private BorderLayout border;
-	
-
-	public NumberLine() {
-		super();
-		setBackground(Color.WHITE);
-		// TODO Add GUI customizations to the panel
-	}
-
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g); // Call JPanel's paintComponent method
-									// to paint the background
-
-		int width = getWidth();
-		int height = getHeight();
+	public static void main(String[] args) {
 		
-		g.setColor(Color.CYAN);
-		g.fillRect((int)(width/12.0), 95, (int)(width*10.0/12.0), 10);
-		g.setColor(Color.BLACK);
-		g.drawLine((int)(width/12.0), 100, (int)(width*11.0/12), 100);
-		g.setFont(new Font("Sans Serif", Font.BOLD, 17));
-		for(int i = 0; i <=10; i++) {
-			g.drawLine((int)(width/12.0 + width*i/12.0), 91, (int)(width/12.0 + width*i/12.0), 109);
-			g.drawString(i * 10 + "", (int)(width/12.0 + width*i/12.0 - 10), 70);
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
 		}
 		
-		
-		
-
-		// TODO Draw on the panel here
+		GUI gui = new GUI();
+		gui.setVisible(true);
 	}
-
 }
