1e3c83c31b2acbaa4d38eddc0b065e669092727a
alek-antic <aantic235@PR05.hhs.win.fuhsd.org>
2015-04-22 14:29:51 -0700

    grid4

--- GridWorldPart4/src/info/girdworld/critters/ChameleonKid.java
+++ GridWorldPart4/src/info/girdworld/critters/ChameleonKid.java
@@ -1,5 +1,6 @@
 package info.girdworld.critters;
 
+import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
 
 import java.util.ArrayList;
@@ -8,7 +9,14 @@ public class ChameleonKid extends ChameleonCritter {
 
 	
 	@Override
-	public ArrayList<Location> getActors(){
-		return getGrid().get(getLocation().getDirectionToward(Location.AHEAD));
+	public ArrayList<Actor> getActors(){
+		ArrayList<Actor> frontback = new ArrayList<Actor>();
+		if(getLocation().getAdjacentLocation(Location.AHEAD) != null)
+			frontback.add(getGrid().get(getLocation().getAdjacentLocation(Location.AHEAD)));
+		if(getLocation().getAdjacentLocation(Location.HALF_CIRCLE) != null)
+			frontback.add(getGrid().get(getLocation().getAdjacentLocation(Location.HALF_CIRCLE)));
+		return frontback;
 	}
+	
+	
 }
